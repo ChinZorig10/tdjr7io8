5d03d8224d1edd2451db93732a2e269849e21e5e
ChinZorig10 <zorigchin78@gmail.com>
2024-09-22 17:12:20 +0800

    jfgw

---
+++ toys.js
@@ -0,0 +1,44 @@
+alert("ta negen undur huuchirsan bairand serlee");
+alert("ta 10 davhart baina ta zugtaj chadval hojino");
+alert("oroond tantai adilhan 13 hun baiva");
+alert("ta bugdiig n daguulj yvah uu/uldeeh uu");
+var songolt1=prompt("daguulah/uldeeh");
+if(songolt1=="daguulah"){
+	alert("9davhart aluurchid shuugildaj bn");
+	alert("ta nar shataar nuutsaar yvah uu/bolkonoor yvah uu");
+var songolt2=prompt("bolkon/shat");
+if(songolt2=="bolkon"){
+alert("ols n 14 huniig daahgui bugd unaj uhlee DEFEAT")
+}else if(songolt2=="shat")
+	alert("ta nariig shuud alahiin orond tamlaj alna gehiig neg emegtei sonsoj yg hashigrahiin davaan deer nuhur n amiig n taglaj ta nar amjilttai 8 davharluu buulaa")
+alert("7,6,5,4,3 davhart gurva gurvan aluurchin baina")
+alert("ta nar dund 6 eregtei 7 emegtei baina ")
+alert("yu yu baigaag ta eregtei chuudee emegtei chuudiig hamgaaluulaad gantsaaraa haih uu bugd haihuu")
+var songolt3=prompt("gantsaaraa/bugd")
+if(songolt3=="gantsaaraa"){
+alert("ta hutga, hulgan olloo")
+alert("ta hutgaa ashiglah uu hulgan aa/ashiglah uu")
+var songolt4=prompt("hulgan/hutga")
+if(songolt4=="hulgan"){
+alert("hulgan doosh buuj 7 davhart baigaa aluurchdiig ailgaj yvuullaa")
+alert("ta 7 davharluu amjilttai buulaa")
+alert("6 davhariin guravd zevseg alga 6eregtei 6 hun zodoldoh/4 hun zodoldoh")
+}else if(songolt4=="hutga")
+alert("dairah aas n buudaj allaa DEFEAT")
+var songolt5=prompt("6 hun/4 hun")
+if(songolt5=="6 hun"){
+alert("zodoldoj ylsanch 6 hun uhlee DEFEAT")
+}else if (songolt5=="4 hun")
+alert("zodoldoj yllaa 4 hun betlee")
+alert("6 davhart aluurchdiin uldeesen buu baini avah uu/hayah uu")
+var songolt6=prompt("avah/hayah")
+if(songolt6=="avah"){
+alert("dood davhar luu shagaihad 3buutai 3 hulganaas aigaad buusan 3")
+}else if (songolt6=="hayah")
+alert("ta nar buug hayahiig sonsoj ta nariig allaa DEFEAT")
+alert("")
+}else if (songolt3=="bugd")
+alert("bujignaldaj baigaag sonsood ta nariig allaa DEFEAT")
+}else if(songolt1=="uldeeh"){
+alert("taniig yvsnii daraa 1 aluurchin irj 14-13 baigaag anzaarch 1 chin haana baina gej asuuhad oorsdiig n hayj yvsan taniig orgosniig helj taniig buudan allaa DEFEAT")
+}
